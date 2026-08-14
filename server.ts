e471da8a9aa635aea5446041e57dbfd0b8412bb2
outslept <135520429+outslept@users.noreply.github.com>
2026-08-14 20:57:04 +0300

    chore: remove old stuff

--- server.ts
+++
@@ -1,6 +0,0 @@
-import app from './src/app.js'
-import { serve } from '@hono/node-server'
-import 'dotenv/config'
-
-const port = parseInt(process.env.PORT || '8080', 10)
-serve({ fetch: app.fetch, port })
